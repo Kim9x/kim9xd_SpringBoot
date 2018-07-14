b642c80e763a6dfe22f4423add6542c70b836ae0
김태주 <kim9xd@gmail.com>
2018-07-14 20:54:16 +0900

    initial commit

---
+++ src/main/java/net/kim9xd/Kim9xdApplication.java
@@ -0,0 +1,12 @@
+package net.kim9xd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kim9xdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kim9xdApplication.class, args);
+	}
+}
