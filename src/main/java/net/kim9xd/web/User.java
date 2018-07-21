61601f88df0534292d9e94e9cae8cf7a0dd048fd
김태주 <kim9xd@gmail.com>
2018-07-21 18:56:18 +0900

    [add - userListPage, joinFunction]  - add userListPage  - add joinFunction  - change to Dynamic Page using mustache

---
+++ src/main/java/net/kim9xd/web/User.java
@@ -0,0 +1,31 @@
+package net.kim9xd.web;
+
+public class User {
+	public String userId;
+	public String password;
+	public String name;
+	public String email;
+	
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", password=" + password + ", name=" + name + ", email=" + email + "]";
+	}
+	
+	public String test() {
+	 	System.out.println("If call User class, run this method?");
+	 	
+	 	return "If call User class, run this method?";
+	}
+}
